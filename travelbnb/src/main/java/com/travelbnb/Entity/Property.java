791d73c3a7b7f2ff5c774c26edca35b60632ba42
br2k-2000 <br2ksahoo@gmail.com>
2025-04-27 01:19:33 +0530

    new changes commit

--- travelbnb/src/main/java/com/travelbnb/Entity/Property.java
+++ travelbnb/src/main/java/com/travelbnb/Entity/Property.java
@@ -7,6 +7,7 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serial;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
@@ -16,6 +17,8 @@ import java.time.LocalDateTime;
 @Table(name = "property")
 @JsonSerialize
 public class Property implements Serializable {
+
+    @Serial
     private static final long serialVersionUID = 1L;
 
     @Id
